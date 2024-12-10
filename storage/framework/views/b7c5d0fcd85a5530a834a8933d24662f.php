b4d1fc72f07cd572cbe82aadf451c09d2da4fab0
moonthemoonman <98581025+QU33NM00N@users.noreply.github.com>
2024-12-10 09:06:21 +0300

    Add files via upload

---
+++ storage/framework/views/b7c5d0fcd85a5530a834a8933d24662f.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WorkSphere</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">WorkSphere</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">YourSphere</a>
+              </li>
+            </ul> 
+
+
+            <form class="d-flex" action=" ">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button style="background-color: rgb(61,157,241); color:aliceblue; border-color: rgb(42,59,95);" class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <section class="container">
+  <div class="bg-image">
+  </div>
+ 
+<h1>
+Welcome to WorkSphere!
+</h1>
+<h4>
+    Your workplace, simplified. Manage teams, track progress, and stay connected—all from one platform. With WorkSphere, the world is your office.
+    <hr>
+    Let’s shape the future of work, together.
+</h4>
+
+</section>
+
+</html><?php /**PATH C:\xampp\htdocs\hrmanage\resources\views/home.blade.php ENDPATH**/ ?>
