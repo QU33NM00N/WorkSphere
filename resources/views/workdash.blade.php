6d7562b3efd9119575550c0c7d24c92ae01dc27b
moonthemoonman <98581025+QU33NM00N@users.noreply.github.com>
2024-12-10 09:03:43 +0300

    Add files via upload

---
+++ resources/views/workdash.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>WorkSphere</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+       
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">WorkSphere</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">YourSphere</a>
+              </li>
+            </ul> 
+
+
+            <form class="d-flex" action=" ">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button style="background-color: rgb(61,157,241); color:aliceblue; border-color: rgb(42,59,95);" class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+</div>
+
+
+      <div class="card-group">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">MVW Nominations for 2024</h5>
+            <p class="card-text">Nominate your Most Valued workmates for this years awards!</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 2 hours ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Office-wide memo</h5>
+            <p class="card-text">Hi guys! Unfortunately enough, it seems that the Kuerig is broken, so we've got a regular coffee pot set up in the meantime!</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 20 mins ago</small>
+          </div>
+        </div>
+    </div>
+<br>
+    <div class="main">
+      <h3>YourSphere Info</h3>
+  </div>
+  <div class="container">
+      <button class="btn"><a href="#" class="text-light">Add New Company</a></button>
+  </div>
+  <div class="container">
+
+
+
+      <table class="table table-bordered" style="margin-left: 100px;">
+          <thead>
+              <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Company Id</th>
+                  <th scope="col">Company Department</th>
+                  <th scope="col">Employee Name</th>
+                  <th scope="col">Employee Email</th>
+              </tr>
+             
+          </thead>
+          <tbody>
+</body>
+</html>
